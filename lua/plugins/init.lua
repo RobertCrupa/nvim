72efc6d12bef681b98b37bae94fda801e1021870
Robert-George Crupa <robertcrupa@outlook.com>
2026-04-15 18:55:46 +0200

    Added markdown preview

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -49,4 +49,15 @@ return {
       automatic_installation = true,
     },
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
